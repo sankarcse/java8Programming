a4f14849187c49b87c8a3eac27237f628405f811
sankar <sankarcse05@.gmail.com>
2025-04-10 12:13:13 +0530

    second smallest edited

--- src/java8Programming/SecondSmallestInArray.java
+++ src/java8Programming/SecondSmallestInArray.java
@@ -1,9 +1,16 @@
 package java8Programming;
 
+import java.util.Arrays;
+
 public class SecondSmallestInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//int[] arr= {5,2,8,3,1,1,1,1};
+		
+		int[] arr= {1};
+		int num=Arrays.stream(arr).distinct().skip(1).limit(1).findFirst().
+				orElseThrow(() -> new IllegalArgumentException("no second smallest"));
+		System.out.println("second smallest"+num);
 
 	}
 
