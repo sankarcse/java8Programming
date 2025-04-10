10c192227c14344d9b59dbe35cb6c17e7b3796b7
sankar <sankarcse05@.gmail.com>
2025-04-10 12:22:06 +0530

    space removed in second smallest of array

--- src/java8Programming/SecondSmallestInArray.java
+++ src/java8Programming/SecondSmallestInArray.java
@@ -6,12 +6,13 @@ public class SecondSmallestInArray {
 
 	public static void main(String[] args) {
 		//int[] arr= {5,2,8,3,1,1,1,1};
-		
 		int[] arr= {1};
 		int num=Arrays.stream(arr).distinct().skip(1).limit(1).findFirst().
 				orElseThrow(() -> new IllegalArgumentException("no second smallest"));
 		System.out.println("second smallest"+num);
 
+		
+		
 	}
 
 }
