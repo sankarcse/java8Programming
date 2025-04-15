2be92808943496116ebd1dffd38a18e1e1952fd1
sankar <sankarcse05@.gmail.com>
2025-04-15 10:45:03 +0530

    factorial and armstrong number

--- src/javaTPointPrograms/_10_FactorialOfNumber.java
+++ src/javaTPointPrograms/_10_FactorialOfNumber.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 public class _10_FactorialOfNumber {
     public static void main(String[] args) {
-        int num=5;
+        int num=0;
 
         int factorial=factorialOfNumber(num);
         System.out.println(factorial);
