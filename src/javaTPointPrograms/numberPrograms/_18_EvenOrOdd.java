d6d65f3ce1e55bcea7b42e3fa88f959b29014dc3
sankar <sankarcse05@.gmail.com>
2025-04-26 13:57:38 +0530

    even odd and sum of natural numbers

---
+++ src/javaTPointPrograms/numberPrograms/_18_EvenOrOdd.java
@@ -0,0 +1,13 @@
+package javaTPointPrograms.numberPrograms;
+
+import java.util.stream.IntStream;
+
+public class _18_EvenOrOdd {
+    public static void main(String[] args) {
+        int number=10;
+        if(number%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+}
